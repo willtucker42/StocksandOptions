9fed6dab33c39299ba19853890d8773b45e65374
willtucker42 <iamhiding24@gmail.com>
2022-02-13 20:38:24 -0800

    Update options_alerterV1.py

--- options_alerterV1.py
+++ options_alerterV1.py
@@ -1,9 +1,9 @@
 # This script scrapes finviz and yahoo finance using selenium
-# It looks for low volatility stocks that are seeing an increase in price from open
-# It then looks for options for that stock that have "bargain" prices
-# If it finds an option chain with bargain prices it sends a push notification to my phone
+# It looks for low volatility stocks that are seeing an increase in price from market open
+# It then looks for options for that stock that have not reacted to the price increase yet
+# If it finds a stock that has good option prices it sends a push notification to my phone
 # Stock criteria for finviz: Optionable, up 3% from open, average volume over 500k, ordered by lowest volatility
-# Options criteria: Calls with strike prices 1-3 levels above underlying stock price with last price <= 0.05 , closest expiration date
+# Options criteria: Calls. Strike prices 10% above above underlying stock price with last price <= 0.05, nearest expiration date
 
 
 from pushbullet import Pushbullet
@@ -15,7 +15,8 @@ options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-gpu")
 options.add_argument("--headless")
-PUSHBULLET_API_KEY = "x"
+PUSHBULLET_API_KEY = "o.sFSmq3qn2L7ZwD3acBTU2eRHGEmgXll3"
+
 
 def getTickerSymbolsForRisingStocks():
     # CRITERIA: Optionable, up 3% from open, average volume over 500k, ordered by lowest volatility
@@ -47,6 +48,7 @@ def getOptionDataForTickerSymbol(ticker):
     except Exception:
         print("\n\n ERROR IN getOptionDataForTickerSymbol \n\n")
         print(traceback.format_exc())
+        driver.close()
         return 0
     try:
         rw_data = driver.find_elements_by_xpath("//table/tbody/tr")
@@ -57,11 +59,15 @@ def getOptionDataForTickerSymbol(ticker):
             class_ = price.get_attribute('class')
             if class_ is not None and class_ == "Fw(b) Fz(36px) Mb(-4px) D(ib)":
                 current_stock_price = float(price.text)
-                print("found it: " + str(current_stock_price))
+                print("Current price: " + str(current_stock_price))
         if current_stock_price == 0:
             print("\n\n UNABLE TO GET CURRENT STOCK PRICE \n\n")
             return
+        upper_strike_limit = current_stock_price + (current_stock_price * 0.1)
+        print("upper strike limit " + str(upper_strike_limit))
         last_strike = 0
+        print("row data count: ")
+        allows_non_multiples_of_five = False
         for row in rw_data:
             row_data = str(row.text).split(' ')
             if "P0" in row_data[0]:
@@ -69,14 +75,20 @@ def getOptionDataForTickerSymbol(ticker):
             # print(row_data)
             strike_price = float(row_data[4])
             last_price = float(row_data[5])
-            if last_strike < current_stock_price < strike_price:
-                if last_price <= 0.4:
+            bid = float(row_data[6])
+            ask = float(row_data[7])
+            if checkIfAllowsNonMultiplesOfFive(last_price, bid, ask):
+                print(ticker + " supports nonMultiplesOfFive!")
+                allows_non_multiples_of_five = True
+            if current_stock_price < strike_price < upper_strike_limit:
+                if last_price <= 0.05 and allows_non_multiples_of_five:  # potentially use multiples of 5 boolean here
                     print("SENDING NOTIF")
                     pb = Pushbullet(PUSHBULLET_API_KEY)
-                    push = pb.push_note('$' + ticker + "("+str(current_stock_price)+")", "" + str(strike_price) + "C - Last price: " + str(last_price))
-            print("Strike: " + str(strike_price) + ", Last price: " + str(last_price))
+                    pb.push_note('$' + ticker + "(" + str(current_stock_price) + ")",
+                                 "" + str(strike_price) + "C - Last price: " + str(last_price))
+
+            # print("Strike: " + str(strike_price) + ", Last price: " + str(last_price))
             last_strike = float(strike_price)
-     print(link.text)
     except Exception:
         print("\n\n ERROR AFTER GETTING PAGE DATA \n\n")
         print(traceback.format_exc())
@@ -85,8 +97,24 @@ def getOptionDataForTickerSymbol(ticker):
     driver.close()
 
 
-# t_list = getTickerSymbolsForRisingStocks()
+def checkIfAllowsNonMultiplesOfFive(last_price, bid, ask):
+    last_price_last_digit = int(str(last_price)[-1])
+    bid_last_digit = int(str(bid)[-1])
+    ask_price_last_digit = int(str(ask)[-1])
+    if ask_price_last_digit != 0 and ask_price_last_digit != 5:
+        return True
+    if bid_last_digit != 0 and bid_last_digit != 5:
+        return True
+    if last_price_last_digit != 0 and last_price_last_digit != 5:
+        return True
+    return False
+
+
+# print(checkIfAllowsNonMultiplesOfFive(5.67, .10, 1.05))
+
+t_list = getTickerSymbolsForRisingStocks()
 
 getOptionDataForTickerSymbol("GOLD")
-# for t in t_list:
-#     getOptionDataForTickerSymbol(t)
+for t in t_list:
+    getOptionDataForTickerSymbol(t)
+
